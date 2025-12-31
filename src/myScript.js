d2dc91734960ae5c9b5895f6122db2b11c3773bc
v-pelaez <victor.pelaez.perez@gmail.com>
2025-12-31 19:19:43 +0100

    add video modal

---
+++ src/myScript.js
@@ -0,0 +1,37 @@
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
+
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      var forms = document.getElementsByClassName("needs-validation");
+      Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
